9d84c6b0eb7beabf9866e0566ec4c0fd3a1a78ad
Mike Selander <mikeselander@gmail.com>
2020-05-11 14:39:34 -0600

    Limit flags in the user metabox to only user options

--- inc/user-metabox.php
+++ inc/user-metabox.php
@@ -20,7 +20,7 @@ function bootstrap() {
  * @param \WP_User $user
  */
 function render( \WP_User $user ) {
-	$flags  = wp_list_filter( Flags::get_all(), [ 'available' => true ] );
+	$flags  = wp_list_filter( Flags::get_all(), [ 'available' => true, 'scope' => 'user' ] );
 	$values = call_user_func_array( 'array_merge', array_map( function ( $flag ) use ( $user ) {
 		/* @var \HumanMade\Flags\Flag $flag */
 		return [ $flag->id => get_user_meta( $user->ID, $flag->get_meta_key(), true ) === 'active' ];
